3f3c050c95f5ed9aa1262780be131631f2011a44
“00Amit0” <amit836961@gmail.com>
2022-05-24 03:00:51 +0530

    Check Employee Present or Not

---
+++ EmployeeCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    internal class EmployeeCheck
+    {
+        public static void EmployeePresentOrNot()
+        {
+            int Is_Full_Time = 1;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
