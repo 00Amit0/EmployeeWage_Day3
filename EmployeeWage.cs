74acc0a3591c802f2418aa0a5e76450c3d75490b
“00Amit0” <amit836961@gmail.com>
2022-05-24 03:17:00 +0530

    Calculate Employee Wage Per Hr

---
+++ EmployeeWage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    internal class EmployeeWage
+    {
+        public static void EmployeeRatePerHr()
+        {
+
+            int Is_Full_Time = 1;
+            int Emp_Rate_Per_Hr = 20;
+            int empwage = 0;
+            int emphrs = 0;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+                emphrs = 8;
+                
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                emphrs = 0;
+            }
+            empwage = emphrs * Emp_Rate_Per_Hr;
+            Console.WriteLine("Emp Wage :" + empwage);
+            Console.ReadKey();
+        }
+    }
+}
