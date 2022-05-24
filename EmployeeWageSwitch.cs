cc7c161b0d23b1848138230fbf257327a7fe6b8e
“00Amit0” <amit836961@gmail.com>
2022-05-24 08:46:49 +0530

    Employee Wage using Switch

--- EmployeeWageSwitch.cs
+++ EmployeeWageSwitch.cs
@@ -8,5 +8,31 @@ namespace EmployeeWageProgram
 {
     internal class EmployeeWageSwitch
     {
+         public const int Is_Full_Time = 1;
+         public const int Is_Part_Time = 2;
+         public const int EMP_RATE_PER_HR = 20;
+        public static void EmployeeWageUsingSwitch()
+        {
+           
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case Is_Full_Time:
+                    empHrs = 16;
+                    break;
+                case Is_Part_Time:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Emp Wage :" + empWage);
+            Console.ReadKey();
+        }
     }
 }
